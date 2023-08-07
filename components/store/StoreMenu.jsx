151c43878d808d9c9dd0db18b18e5837e5f30e9a
rkdghwnd <ghwnd6448@gmail.com>
2023-08-07 21:29:41 +0900

    [feat] implement store banner

---
+++ components/store/StoreMenu.jsx
@@ -0,0 +1,38 @@
+import Link from 'next/link';
+import React from 'react';
+import Image from 'next/image';
+import { storeMenus1, storeMenus2 } from '../../hooks/storeMenus';
+import shortid from 'shortid';
+
+const StoreMenu = () => {
+  return (
+    <section className="store-menu">
+      <div className="store-menu-row-1">
+        {storeMenus1.map((menu) => {
+          return (
+            <Link href={menu.href} key={shortid.generate()}>
+              <div className="store-menu-item">
+                <img src={menu.src} loading="lazy" />
+                <span>{menu.title}</span>
+              </div>
+            </Link>
+          );
+        })}
+      </div>
+      <div className="store-menu-row-2">
+        {storeMenus2.map((menu) => {
+          return (
+            <Link href={menu.href} key={shortid.generate()}>
+              <div className="store-menu-item">
+                <img src={menu.src} loading="lazy" />
+                <span>{menu.title}</span>
+              </div>
+            </Link>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default StoreMenu;
