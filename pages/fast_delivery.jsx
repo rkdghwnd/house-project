470ec5c84256ee8a23dbc809fc1a7790a3af7860
rkdghwnd <ghwnd6448@gmail.com>
2023-08-09 22:52:49 +0900

    [feat] add exhibitions,fast delivery,oh goods page

---
+++ pages/fast_delivery.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import AppLayout from '../components/Common/AppLayout';
+
+const fast_delivery = () => {
+  return <AppLayout></AppLayout>;
+};
+
+export default fast_delivery;
---
+++ pages/oh_goods.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import AppLayout from '../components/Common/AppLayout';
+
+const oh_goods = () => {
+  return <AppLayout></AppLayout>;
+};
+
+export default oh_goods;
