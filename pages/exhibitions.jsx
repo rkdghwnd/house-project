598be74167f1815c52d650366674b45174ef47b5
rkdghwnd <ghwnd6448@gmail.com>
2023-08-11 22:23:37 +0900

    [fix] merge conflict of feat

--- pages/exhibitions.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import AppLayout from '../components/Common/AppLayout';
-
-const exhibitions = () => {
-  return <AppLayout></AppLayout>;
-};
-
-export default exhibitions;
--- pages/fast_delivery.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import AppLayout from '../components/Common/AppLayout';
-
-const fast_delivery = () => {
-  return <AppLayout></AppLayout>;
-};
-
-export default fast_delivery;
