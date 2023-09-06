9e3cf082367547a042bd08e0e949cd22191aeccd
rkdghwnd <ghwnd6448@gmail.com>
2023-09-07 08:46:25 +0900

    [feat] implement production reviews write

--- front/pages/ProductionReviewsWrite.jsx
+++ front/pages/ProductionReviewsWrite.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
 
 const ProductionReviewsWrite = () => {
-  return <div></div>;
+  return (
+    <section className="production-reviews-write">
+      <article className="production-reviews-write-main">
+        <h4>내가 사용하는 상품 리뷰쓰기</h4>
+        <div className="search-reviews-input">
+          <input type="text" placeholder="브랜드명 혹은 상품명 입력" />
+          <button className="btn-primary btn-40">검색</button>
+        </div>
+      </article>
+    </section>
+  );
 };
 
 export default ProductionReviewsWrite;
