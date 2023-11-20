bef5d550d33daaa316dd9979a1869592133bdf8f
rkdghwnd <ghwnd6448@gmail.com>
2023-11-21 01:14:03 +0900

    [feat] TodayDealsMain

--- front/components/today_deals/TodayDealsMain.jsx
+++ front/components/today_deals/TodayDealsMain.jsx
@@ -1,7 +1,56 @@
-import React from 'react';
-import StoreItem from '../home/StoreItem';
+import React, { useEffect, useRef } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getTodayDealsProducts } from '../../actions/product';
+import HotdealStoreItem from '../common/HotdealStoreItem';
+import shortid from 'shortid';
+import { createInfiniteScrollObserver } from '../../hooks/createInfiniteScrollObserver';
+import { LOADING } from '../../datas/statusConstants';
+import Spinner from '../../loading/spinner';
+import Loading from '../common/Loading';
 
 const TodayDealsMain = () => {
+  const dispatch = useDispatch();
+  const viewport = useRef(null);
+  const scrollTarget = useRef(null);
+  const {
+    getTodayDealsProductsStatus,
+    todayDealsProducts,
+    hasMoreTodayDealsProducts,
+  } = useSelector((state) => state.product);
+
+  useEffect(() => {
+    dispatch(
+      getTodayDealsProducts({
+        query: '?page=1',
+      })
+    );
+  }, []);
+
+  useEffect(() => {
+    // 페이지 + 배송필터 + 정렬필더
+    const page = Math.floor(todayDealsProducts.length / 12) + 1;
+
+    const query = `?page=${page}`;
+
+    const io = createInfiniteScrollObserver(
+      viewport,
+      hasMoreTodayDealsProducts,
+      getTodayDealsProductsStatus,
+      scrollTarget,
+      dispatch,
+      getTodayDealsProducts,
+      query
+    );
+
+    return () => io && io.disconnect(); // 모든 요소의 관찰을 중지
+  }, [
+    todayDealsProducts,
+    viewport,
+    scrollTarget,
+    getTodayDealsProductsStatus,
+    hasMoreTodayDealsProducts,
+  ]);
+
   return (
     <section className="today-deals-main">
       <div className="today-deals-main-header sm-hidden">
@@ -9,25 +58,19 @@ const TodayDealsMain = () => {
         <span>매일 자정,새로운 특가상품</span>
       </div>
       <div className="today-deals-main-items">
-        <div className="today-deals-main-items-col">
-          <StoreItem />
-          <StoreItem />
-          <StoreItem />
-          <StoreItem />
-        </div>
-        <div className="today-deals-main-items-col">
-          <StoreItem />
-          <StoreItem />
-          <StoreItem />
-          <StoreItem />
-        </div>
-        <div className="today-deals-main-items-col">
-          <StoreItem />
-          <StoreItem />
-          <StoreItem />
-          <StoreItem />
-        </div>
+        {todayDealsProducts.map((product) => {
+          return <HotdealStoreItem key={shortid.generate()} {...product} />;
+        })}
       </div>
+      <Loading loadProductsStatus={getTodayDealsProductsStatus} />
+      <div
+        ref={
+          hasMoreTodayDealsProducts &&
+          !(getTodayDealsProductsStatus === LOADING)
+            ? scrollTarget
+            : undefined
+        }
+      />
     </section>
   );
 };
