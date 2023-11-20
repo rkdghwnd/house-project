b26c5a5a88bcb6edcc016f6c695c822b968262d1
rkdghwnd <ghwnd6448@gmail.com>
2023-11-21 01:11:42 +0900

    [feat] ScoreStats

--- front/components/productions/ScoreStats.jsx
+++ front/components/productions/ScoreStats.jsx
@@ -1,73 +1,51 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import shortid from 'shortid';
+import { getReviewScoreStats } from '../../actions/productions';
 
 const ScoreStats = () => {
+  const dispatch = useDispatch();
+  const { productions, reviewScoreStats } = useSelector(
+    (state) => state.productions
+  );
+
+  useEffect(() => {
+    if (productions.id) {
+      dispatch(
+        getReviewScoreStats({
+          id: productions.id,
+        })
+      );
+    }
+  }, [productions.id]);
+
   return (
     <dl className="score-stats-list">
-      <div className="score-stats-item is-active">
-        <dt>5점</dt>
-        <dd>
-          <div className="bar-graph" aria-hidden>
-            <div
-              className="active-bar"
-              style={{ width: '82.5088339223%' }}
-            ></div>
+      {reviewScoreStats?.starCountArray?.map((starCount, index) => {
+        return (
+          <div
+            key={shortid.generate()}
+            className={`score-stats-item ${
+              reviewScoreStats?.mostCountedStar === index && 'is-active'
+            }`}
+          >
+            <dt>{5 - index}점</dt>
+            <dd>
+              <div className="bar-graph" aria-hidden>
+                <div
+                  className="active-bar"
+                  style={{
+                    width: `${
+                      (starCount / reviewScoreStats.totalCount) * 100
+                    }%`,
+                  }}
+                ></div>
+              </div>
+              <strong className="count">{starCount}</strong>
+            </dd>
           </div>
-          <strong className="count" aria-label="467명">
-            467
-          </strong>
-        </dd>
-      </div>
-
-      <div className="score-stats-item">
-        <dt>4점</dt>
-        <dd>
-          <div className="bar-graph" aria-hidden>
-            <div
-              className="active-bar"
-              style={{ width: '15.371024735%' }}
-            ></div>
-          </div>
-          <strong className="count" aria-label="87명">
-            87
-          </strong>
-        </dd>
-      </div>
-
-      <div className="score-stats-item">
-        <dt>3점</dt>
-        <dd>
-          <div className="bar-graph" aria-hidden>
-            <div className="active-bar" style={{ width: '2.296819788%' }}></div>
-          </div>
-          <strong className="count" aria-label="13명">
-            13
-          </strong>
-        </dd>
-      </div>
-
-      <div className="score-stats-item">
-        <dt>2점</dt>
-        <dd>
-          <div className="bar-graph" aria-hidden>
-            <div className="active-bar"></div>
-          </div>
-          <strong className="count" aria-label="0명">
-            0
-          </strong>
-        </dd>
-      </div>
-
-      <div className="score-stats-item">
-        <dt>1점</dt>
-        <dd>
-          <div className="bar-graph" aria-hidden>
-            <div className="active-bar"></div>
-          </div>
-          <strong className="count" aria-label="0명">
-            0
-          </strong>
-        </dd>
-      </div>
+        );
+      })}
     </dl>
   );
 };
