115b378564683a91ad59e00aee198bfa7ce3cdb0
rkdghwnd <ghwnd6448@gmail.com>
2023-11-21 01:19:08 +0900

    [feat] reducer index.js

--- front/reducers/index.js
+++ front/reducers/index.js
@@ -1,14 +1,26 @@
 import { combineReducers } from 'redux';
 import userSlice from './userSlice';
-import postSlice from './postSlice';
+import productSlice from './productSlice';
 import modalSlice from './modalSlice';
 import sideBarMenuSlice from './sideBarMenuSlice';
+import productionsSlice from './productionsSlice';
+import exhibitionsSlice from './exhibitionsSlice';
+import searchSlice from './searchSlice';
+import finalorderSlice from './finalorderSlice';
+import toastSlice from './toastSlice';
+import paymentSlice from './paymentSlice';
 
 const rootReducer = combineReducers({
   user: userSlice.reducer,
-  post: postSlice.reducer,
+  product: productSlice.reducer,
   modal: modalSlice.reducer,
   sideBarMenu: sideBarMenuSlice.reducer,
+  productions: productionsSlice.reducer,
+  exhibitions: exhibitionsSlice.reducer,
+  search: searchSlice.reducer,
+  finalorder: finalorderSlice.reducer,
+  toast: toastSlice.reducer,
+  payment: paymentSlice.reducer,
 });
 
 export default rootReducer;
