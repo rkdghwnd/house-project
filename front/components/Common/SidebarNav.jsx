02c39bcffcde6e26c098c9050397e8ce2c698484
rkdghwnd <ghwnd6448@gmail.com>
2023-11-21 00:15:31 +0900

    [feat] SIdebarNav

--- front/components/Common/SidebarNav.jsx
+++ front/components/Common/SidebarNav.jsx
@@ -3,17 +3,18 @@ import CommunityDrawerMenu from './CommunityDrawerMenu';
 import StoreDrawerMenu from './StoreDrawerMenu';
 import ExpertDrawerMenu from './ExpertDrawerMenu';
 import SideBarUserMenu from './SideBarUserMenu';
+import { useSelector } from 'react-redux';
 
 const SidebarNav = () => {
+  const { me } = useSelector((state) => state.user);
   return (
     <nav className="sidebar-nav">
       <h2 className="visually-hidden">메뉴</h2>
       {/* <CommunityDrawerMenu /> */}
       <StoreDrawerMenu />
-      <ExpertDrawerMenu />
+      {/* <ExpertDrawerMenu /> */}
 
-      {/* <!-- NOTE: 로그인을 한 경우  --> */}
-      <SideBarUserMenu />
+      {me && <SideBarUserMenu />}
     </nav>
   );
 };
