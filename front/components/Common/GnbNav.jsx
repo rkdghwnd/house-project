6cdff21d6de8d1e27bc787994a5a59f7e3f77ae0
rkdghwnd <ghwnd6448@gmail.com>
2023-11-21 00:57:08 +0900

    [feat] GnbNav

--- front/components/Common/GnbNav.jsx
+++ front/components/Common/GnbNav.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { storeLnb } from '../../hooks/lnb';
+import { storeLnb } from '../../datas/lnb';
 import { Link, useLocation } from 'react-router-dom';
 
 const GnbNav = () => {
