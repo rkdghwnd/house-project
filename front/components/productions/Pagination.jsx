5f9eef0ec26dd421639959bfd57905cf41e9860b
rkdghwnd <ghwnd6448@gmail.com>
2023-11-21 01:09:39 +0900

    [feat] Pagination

--- front/components/productions/Pagination.jsx
+++ front/components/productions/Pagination.jsx
@@ -1,26 +1,65 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import shortid from 'shortid';
 
-const Pagination = () => {
+const Pagination = ({
+  totalCount,
+  countPerPage = 5,
+  actionFunction,
+  productId,
+  page,
+  setPage,
+}) => {
+  const dispatch = useDispatch();
+  const pageCountArray = Array.from(
+    { length: Math.ceil(totalCount / countPerPage) || 1 },
+    (_, i) => i + 1
+  );
+
+  const onClickPageButton = useCallback(
+    (e) => {
+      setPage(parseInt(e.currentTarget.value));
+
+      dispatch(
+        actionFunction({
+          query: `?page=${e.currentTarget.value}`,
+          productId,
+        })
+      );
+    },
+    [productId]
+  );
+
   return (
     <div className="pagination">
+      <button
+        className={`page-control page-prev${page === 1 ? ' is-disabled' : ''}`}
+        type="button"
+      >
+        <i className="ic-chevron"></i>
+      </button>
+
       <ol className="page-list">
-        <li className="page-item is-active">
-          <a href="/">1</a>
-        </li>
-        <li className="page-item">
-          <a href="/">2</a>
-        </li>
-        <li className="page-item">
-          <a href="/">3</a>
-        </li>
-        <li className="page-item">
-          <a href="/">4</a>
-        </li>
-        <li className="page-item">
-          <a href="/">5</a>
-        </li>
+        {pageCountArray?.map((pageNumber) => {
+          return (
+            <li
+              className={`page-item${page === pageNumber ? ' is-active' : ''}`}
+              key={shortid.generate()}
+            >
+              <button value={pageNumber} onClick={onClickPageButton}>
+                {pageNumber}
+              </button>
+            </li>
+          );
+        })}
       </ol>
-      <button className="page-control page-next" type="button">
+
+      <button
+        className={`page-control page-next${
+          page === pageCountArray.length ? ' is-disabled' : ''
+        }`}
+        type="button"
+      >
         <i className="ic-chevron"></i>
       </button>
     </div>
