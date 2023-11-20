4b9b194a9b8453411c026f2823ca0dad38b6c7e8
rkdghwnd <ghwnd6448@gmail.com>
2023-11-21 01:09:11 +0900

    [feat] OrderFormModal

--- front/components/productions/OrderFormModal.jsx
+++ front/components/productions/OrderFormModal.jsx
@@ -1,158 +1,135 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useCallback } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import OrderCheckouts from './OrderCheckouts';
+import OrderInputs from './OrderInputs';
+import OrderSummary from './OrderSummary';
+import modalSlice from '../../reducers/modalSlice';
+import { useNavigate } from 'react-router-dom';
+import { addFinalOrder } from '../../actions/finalorder';
+import productSlice from '../../reducers/productSlice';
 
 const OrderFormModal = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { me } = useSelector((state) => state.user);
   const { orderFormModalVisible } = useSelector((state) => state.modal);
+  const { selectedOptions, productions } = useSelector(
+    (state) => state.productions
+  );
+
+  const addInCart = useCallback(() => {
+    const cartOptionDatas = selectedOptions
+      .filter((option) => option.optionCount)
+      .map((option) => {
+        return {
+          optionName: option.optionName,
+          optionCount: option.optionCount,
+          productId: productions.id,
+        };
+      });
+    if (cartOptionDatas.length === 0) {
+      dispatch(
+        modalSlice.actions.openMessageModal({
+          message: '상품을 선택해주세요',
+        })
+      );
+    } else {
+      dispatch(
+        productSlice.actions.addProductInBrowserCart({
+          cartOptionDatas,
+          productions,
+        })
+      );
+      dispatch(modalSlice.actions.openCartModal());
+      dispatch(productSlice.actions.updateBroswerCartProductCount());
+    }
+  }, [selectedOptions, productions.id]);
+
+  const buyNow = useCallback(() => {
+    if (me) {
+      // 로그인 했을 때
+      const countedOptions = selectedOptions.filter(
+        (option) => option.optionCount > 0
+      );
+      if (countedOptions.length === 0) {
+        // 상품을 선택하지 않은 경우
+        dispatch(
+          modalSlice.actions.openMessageModal({
+            message: '상품을 선택해주세요',
+          })
+        );
+      } else {
+        const selectedProduct = {
+          id: productions.id,
+          product_name: productions.product_name,
+          brand_name: productions.brand_name,
+          image_url: productions.image_url,
+          free_delivery: productions.free_delivery,
+          selling_price: productions.selling_price,
+        };
+        // 선택한 상품을 final_order에 추가하고 navigate('/final_order')
+        const optionData = selectedOptions
+          .filter((option) => option.optionCount > 0)
+          .map(({ id, optionCount, optionName }) => {
+            return {
+              id: id,
+              product_count: optionCount,
+              product_option: optionName,
+              UserId: me.id,
+              ProductId: productions.id,
+            };
+          });
+        selectedProduct.Cart_product = optionData;
+
+        dispatch(
+          addFinalOrder({
+            cart: [selectedProduct],
+          })
+        ).then((result) => {
+          if (addFinalOrder.fulfilled.match(result)) {
+            navigate('/final_order');
+            dispatch(modalSlice.actions.closeModal());
+          }
+        });
+      }
+    } else {
+      // 로그인 안 했을 때
+      dispatch(modalSlice.actions.openLogInModal());
+    }
+  }, [me, selectedOptions, productions]);
+
   return (
     <aside
-      className={`order-form-modal lg-hidden ${
-        orderFormModalVisible ? 'is-open' : ''
+      className={`order-form-modal lg-hidden${
+        orderFormModalVisible ? ' is-open' : ''
       }`}
     >
       <h1 className="visually-hidden">주문창</h1>
 
-      <form className="order-form" action="/" method="POST">
+      <section className="order-form">
         <div className="container">
           <div className="row">
             <div className="col-sm-4">
-              <div className="order-inputs">
-                <div className="select-group is-active">
-                  <select
-                    className="form-select"
-                    id="order-form-modal-select-1"
-                    required
-                  >
-                    <option value="">선택</option>
-                    <option value="1">선택사항 1</option>
-                    <option value="2">선택사항 2</option>
-                    <option value="3">선택사항 3</option>
-                    <option value="4">선택사항 4</option>
-                    <option value="5">선택사항 5</option>
-                  </select>
-                  <i className="ic-caret" aria-hidden></i>
-                </div>
-                <div className="select-group">
-                  <select
-                    className="form-select"
-                    id="order-form-modal-select-2"
-                  >
-                    <option value="">추가상품 (선택)</option>
-                    <option value="1">선택사항 1</option>
-                    <option value="2">선택사항 2</option>
-                    <option value="3">선택사항 3</option>
-                    <option value="4">선택사항 4</option>
-                    <option value="5">선택사항 5</option>
-                  </select>
-                  <i className="ic-caret" aria-hidden></i>
-                </div>
-              </div>
-
-              <div className="order-checkouts">
-                <ul className="checkout-list">
-                  <li className="checkout-item">
-                    <div className="checkout-card">
-                      <header className="checkout-header">
-                        <h4 className="checkout-title">화이트</h4>
-                        <button
-                          className="delete-button"
-                          type="button"
-                          aria-label="해당 상품을 삭제하기"
-                        >
-                          <i className="ic-close" aria-hidden></i>
-                        </button>
-                      </header>
-
-                      <footer className="checkout-footer">
-                        <div className="checkout-select">
-                          <select id="order-form-modal-checkout-item-1">
-                            <option value="1">1</option>
-                            <option value="2">2</option>
-                            <option value="3">3</option>
-                            <option value="4">4</option>
-                            <option value="5">5</option>
-                          </select>
-                          <i className="ic-caret" aria-hidden></i>
-                        </div>
-
-                        <output
-                          className="checkout-output"
-                          htmlFor="order-form-modal-checkout-item-1"
-                        >
-                          <div className="price-16">
-                            <strong className="amount">32,900</strong>
-                            <span className="currency">원</span>
-                          </div>
-                        </output>
-                      </footer>
-                    </div>
-                  </li>
-                  <li className="checkout-item">
-                    <div className="checkout-card">
-                      <header className="checkout-header">
-                        <h4 className="checkout-title">
-                          추가상품 - 귤무덤 패키지 / 제주감귤 3kg / 온라인 감귤
-                          판매 1위
-                        </h4>
-                        <button
-                          className="delete-button"
-                          type="button"
-                          aria-label="해당 상품을 삭제하기"
-                        >
-                          <i className="ic-close" aria-hidden></i>
-                        </button>
-                      </header>
-
-                      <footer className="checkout-footer">
-                        <div className="checkout-select">
-                          <select id="order-form-modal-checkout-item-2">
-                            <option value="1">1</option>
-                            <option value="2">2</option>
-                            <option value="3">3</option>
-                            <option value="4">4</option>
-                            <option value="5">5</option>
-                          </select>
-                          <i className="ic-caret" aria-hidden></i>
-                        </div>
-
-                        <output
-                          className="checkout-output"
-                          htmlFor="order-form-modal-checkout-item-2"
-                        >
-                          <div className="price-16">
-                            <strong className="amount">30,000</strong>
-                            <span className="currency">원</span>
-                          </div>
-                        </output>
-                      </footer>
-                    </div>
-                  </li>
-                </ul>
-              </div>
-
-              <dl className="order-summary">
-                <dt>주문금액</dt>
-                <dd>
-                  <output htmlFor="order-form-modal-select-1 order-form-modal-select-2">
-                    <div className="price-20">
-                      <strong className="amount">62,900</strong>
-                      <span className="currency">원</span>
-                    </div>
-                  </output>
-                </dd>
-              </dl>
+              <OrderInputs />
+              <OrderCheckouts />
+              <OrderSummary />
             </div>
           </div>
         </div>
 
         <div className="button-group">
-          <button className="btn-secondary btn-48" type="button">
+          <button
+            className="btn-secondary btn-48"
+            type="button"
+            onClick={addInCart}
+          >
             장바구니
           </button>
-          <button className="btn-primary btn-48" type="submit">
+          <button className="btn-primary btn-48" type="submit" onClick={buyNow}>
             구매하기
           </button>
         </div>
-      </form>
+      </section>
     </aside>
   );
 };
