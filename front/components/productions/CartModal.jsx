95b8e2c3a6a61f2952aa14357791a4e06a738b2e
rkdghwnd <ghwnd6448@gmail.com>
2023-11-21 01:08:31 +0900

    [feat] CartModal

--- front/components/productions/CartModal.jsx
+++ front/components/productions/CartModal.jsx
@@ -1,12 +1,19 @@
 import React, { useCallback } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import modalSlice from '../../reducers/modalSlice';
+import { Link, useNavigate } from 'react-router-dom';
 
 const CartModal = () => {
   const dispatch = useDispatch();
   const { cartModalVisible } = useSelector((state) => state.modal);
+  const navigate = useNavigate();
   const closeCartModal = useCallback(() => {
     dispatch(modalSlice.actions.closeModal());
+  }, []);
+
+  const redirectCartPage = useCallback(() => {
+    dispatch(modalSlice.actions.closeModal());
+    navigate('/cart');
   }, []);
 
   return (
@@ -14,10 +21,9 @@ const CartModal = () => {
       <h1 className="cart-title">장바구니에 상품을 담았습니다</h1>
 
       <div className="button-group">
-        <a className="btn-48 btn-primary" href="/">
-          {' '}
-          장바구니 보러가기{' '}
-        </a>
+        <button className="btn-48 btn-primary" onClick={redirectCartPage}>
+          장바구니 보러가기
+        </button>
         <button
           className="btn-48 btn-secondary"
           type="button"
