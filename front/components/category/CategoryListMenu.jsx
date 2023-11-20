e5623a0df33cc4593ffa3d1367fae34e4b4edc40
rkdghwnd <ghwnd6448@gmail.com>
2023-11-21 00:08:58 +0900

    [feat] CategoryListMenu

--- front/components/category/CategoryListMenu.jsx
+++ front/components/category/CategoryListMenu.jsx
@@ -1,17 +1,30 @@
-import React, { useCallback, useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useCallback, useEffect, useState } from 'react';
+import { Link, useLocation, useParams } from 'react-router-dom';
+import shortid from 'shortid';
 
-const CategoryListMenu = () => {
-  const [openSubMenu, setOpenSubMenu] = useState();
+const CategoryListMenu = ({ title, subCategories, indexUnit }) => {
+  const location = useLocation();
+  const [openSubMenu, setOpenSubMenu] = useState(false);
   const toggleSubMenu = useCallback(() => {
     setOpenSubMenu((state) => !state);
   }, []);
+
+  // 해당하는 페이지가 현재 경로랑 같으면? -> is-active 추가하기
+  // 해당하는 페이지가 indexUnit 범위에 있으면? -> 하위메뉴 op-open 추가
+
+  useEffect(() => {
+    if (parseInt(parseInt(location.search.split('=')[1]) / 10) === indexUnit) {
+      setOpenSubMenu(true);
+    } else {
+      setOpenSubMenu(false);
+    }
+  }, [location, indexUnit]);
 
   return (
     <div className="category-list-menu">
       <div className="category-list-menu-header">
         <h2>
-          <Link to="/">가구</Link>
+          <Link to={`/category?category_id=${indexUnit * 10}`}>{title}</Link>
         </h2>
         <i
           className={`ic-chevron ${openSubMenu ? ' on-open' : ''}`}
@@ -19,37 +32,23 @@ const CategoryListMenu = () => {
         ></i>
       </div>
 
-      <ul className={`category-list-menu-sub ${openSubMenu ? ' on-open' : ''}`}>
-        <li className="category-list-menu-sub-link is-active">
-          <Link to="/">침대</Link>
-        </li>
-        <li className="category-list-menu-sub-link">
-          <Link to="/">매트리스·토퍼</Link>
-        </li>
-        <li className="category-list-menu-sub-link">
-          <Link to="/">테이블·식탁·책상</Link>
-        </li>
-        <li className="category-list-menu-sub-link">
-          <Link to="/">소파</Link>
-        </li>
-        <li className="category-list-menu-sub-link">
-          <Link to="/">서랍·수납장</Link>
-        </li>
-        <li className="category-list-menu-sub-link">
-          <Link to="/">거실장·TV장 </Link>
-        </li>
-        <li className="category-list-menu-sub-link">
-          <Link to="/">선반</Link>
-        </li>
-        <li className="category-list-menu-sub-link">
-          <Link to="/">진열장·책장</Link>
-        </li>
-        <li className="category-list-menu-sub-link">
-          <Link to="/">의자</Link>
-        </li>
-        <li className="category-list-menu-sub-link">
-          <Link to="/">헹거·옷장</Link>
-        </li>
+      <ul className={`category-list-menu-sub${openSubMenu ? ' on-open' : ''}`}>
+        {subCategories.map((category, i) => {
+          return (
+            <li
+              className={`category-list-menu-sub-link${
+                parseInt(location.search.split('=')[1]) === indexUnit * 10 + i
+                  ? ' is-active'
+                  : ''
+              }`}
+              key={shortid.generate()}
+            >
+              <Link to={`/category?category_id=${indexUnit * 10 + i}`}>
+                {category}
+              </Link>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
