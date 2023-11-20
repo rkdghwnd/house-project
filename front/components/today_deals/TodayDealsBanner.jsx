939e9cecd8a3de8e58b532326d389587d6ec375e
rkdghwnd <ghwnd6448@gmail.com>
2023-11-21 01:13:54 +0900

    [feat] TodayDealsBanner

--- front/components/today_deals/TodayDealsBanner.jsx
+++ front/components/today_deals/TodayDealsBanner.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 const TodayDealsBanner = () => {
   return (
     <div className="today-deals-banner sm-only">
-      <img src="/assets/images/banner/today_deal_banner.avif" loading="lazy" />
+      <img
+        src="/assets/images/banner/today_deals_banner.avif"
+        alt="오늘의딜 배너"
+      />
     </div>
   );
 };
