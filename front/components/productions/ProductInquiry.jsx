62e3f4eb6c4cbd364c2802b49fe0144556a3abeb
rkdghwnd <ghwnd6448@gmail.com>
2023-11-21 01:10:18 +0900

    [feat] ProductInquiry

--- front/components/productions/ProductInquiry.jsx
+++ front/components/productions/ProductInquiry.jsx
@@ -1,7 +1,111 @@
-import React, { forwardRef } from 'react';
+import React, {
+  forwardRef,
+  useCallback,
+  useEffect,
+  useMemo,
+  useState,
+} from 'react';
 import InquiryCard from './InquiryCard';
+import { useDispatch, useSelector } from 'react-redux';
+import shortid from 'shortid';
+import Pagination from './Pagination';
+import { getProductionInquiry } from '../../actions/productions';
+import modalSlice from '../../reducers/modalSlice';
+import productionsSlice from '../../reducers/productionsSlice';
+import { LOADING, SUCCEEDED } from '../../datas/statusConstants';
 
 const ProductInquiry = (props, ref) => {
+  const dispatch = useDispatch();
+  const { me } = useSelector((state) => state.user);
+  const {
+    productionInquiry,
+    productions,
+    writeInquiryStatus,
+    updateInquiryStatus,
+    removeInquiryStatus,
+  } = useSelector((state) => state.productions);
+  const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    if (productions?.id) {
+      dispatch(
+        getProductionInquiry({
+          productId: productions.id,
+          query: `?page=1`,
+        })
+      );
+    }
+  }, [productions.id]);
+
+  useEffect(() => {
+    if (writeInquiryStatus === SUCCEEDED) {
+      dispatch(
+        getProductionInquiry({
+          productId: productions.id,
+          query: `?page=1`,
+        })
+      );
+      setPage(1);
+    }
+  }, [writeInquiryStatus]);
+
+  useEffect(() => {
+    if (updateInquiryStatus === SUCCEEDED) {
+      dispatch(
+        getProductionInquiry({
+          productId: productions.id,
+          query: `?page=${page}`,
+        })
+      );
+    }
+  }, [updateInquiryStatus]);
+
+  useEffect(() => {
+    if (removeInquiryStatus === SUCCEEDED) {
+      dispatch(
+        getProductionInquiry({
+          productId: productions.id,
+          query: `?page=${page}`,
+        })
+      );
+    }
+  }, [removeInquiryStatus]);
+
+  const openWritingInquiryForm = useCallback(() => {
+    if (me) {
+      dispatch(
+        productionsSlice.actions.updateWritingInquiryFormData({
+          id: null,
+          question_type: '상품',
+          question: '',
+          is_secret: false,
+          productId: productions.id,
+        })
+      );
+      dispatch(
+        modalSlice.actions.openWritingInquiryForm({
+          mode: 'create',
+        })
+      );
+    } else {
+      dispatch(modalSlice.actions.openLogInModal());
+    }
+  }, [me, productions.id]);
+
+  const inquiryList = useMemo(() => {
+    if (productionInquiry.inquiry?.length === 0) {
+      return <p className="inquiry-empty">문의 내역이 없습니다.</p>;
+    } else {
+      return productionInquiry?.inquiry?.map((question) => {
+        return (
+          <li key={shortid.generate()} className="inquiry-item">
+            <InquiryCard {...question} />
+          </li>
+        );
+      });
+    }
+  }, [productionInquiry.inquiry]);
+
   return (
     <>
       <section
@@ -13,12 +117,12 @@ const ProductInquiry = (props, ref) => {
         <header className="product-section-header">
           <h1 className="title">문의</h1>
           <strong className="badge" aria-label="96개">
-            96
+            {productionInquiry?.inquiry?.length}
           </strong>
 
-          <a className="text-button" href="/">
+          <button className="text-button" onClick={openWritingInquiryForm}>
             문의하기
-          </a>
+          </button>
           <button
             className="icon-button sm-only"
             type="button"
@@ -29,157 +133,19 @@ const ProductInquiry = (props, ref) => {
         </header>
 
         <div className="product-section-content">
-          <ol className="inquiry-list">
-            <li className="inquiry-item">
-              <InquiryCard />
-            </li>
-
-            {/* <!-- NOTE: 비밀글일 경우 --> */}
-            <li className="inquiry-item">
-              <article className="inquiry-card">
-                <header className="inquiry-card-header">
-                  <h3 className="visually-hidden">지* 님이 남기신 문의</h3>
-
-                  <dl className="detail">
-                    <dt className="visually-hidden">구매 여부</dt>
-                    <dd>비구매</dd>
-                    <dt className="visually-hidden">문의 유형</dt>
-                    <dd>상품</dd>
-                    <dt className="visually-hidden">답변 여부</dt>
-                    <dd>미답변</dd>
-                  </dl>
-
-                  <div className="misc">
-                    <strong>홍시*</strong>
-                    <time dateTime="2021-01-01 01:01">
-                      2021년 1월 1일 01시 01분
-                    </time>
-                  </div>
-                </header>
-
-                <div className="inquiry-card-body">
-                  <div className="inquiry-content">
-                    <span aria-label="문의 내용">Q</span>
-                    <p>
-                      <i className="ic-lock" aria-hidden></i>
-                      비밀글입니다.
-                    </p>
-                  </div>
-                </div>
-              </article>
-            </li>
-
-            {/* <!-- NOTE: 답변이 달린 경우 --> */}
-            <li className="inquiry-item">
-              <article className="inquiry-card">
-                <header className="inquiry-card-header">
-                  <h3 className="visually-hidden">지* 님이 남기신 문의</h3>
-
-                  <dl className="detail">
-                    <dt className="visually-hidden">구매 여부</dt>
-                    <dd>비구매</dd>
-                    <dt className="visually-hidden">문의 유형</dt>
-                    <dd>배송</dd>
-                    <dt className="visually-hidden">답변 여부</dt>
-                    <dd className="is-answered">답변완료</dd>
-                  </dl>
-
-                  <div className="misc">
-                    <strong>샤*</strong>
-                    <time dateTime="2020-12-24 09:03">
-                      2020년 12월 24일 09시 03분
-                    </time>
-                  </div>
-                </header>
-
-                <div className="inquiry-card-body">
-                  <div className="inquiry-content">
-                    <span aria-label="문의 내용">Q</span>
-                    <p>
-                      화이트 재입고 날짜 알려 주세요 그리고 4평 남짓 방에 두려고
-                      하는데 너무 가까울까요
-                    </p>
-                  </div>
-
-                  <div className="inquiry-content">
-                    <span aria-label="답변 내용">A</span>
-                    <div className="seller">
-                      <strong>OA</strong>
-                      <time dateTime="2020-12-24 19:30">
-                        2020년 12월 24일 19시 30분
-                      </time>
-                    </div>
-                    <p>
-                      안녕하세요 고객님, 보아르입니다:)
-                      <br />
-                      현재 고객님의 주문 정보가 확인되지 않아 바로 안내드리지
-                      못하는 점 양해 부탁드립니다.
-                      <br />
-                      문의하신 제품의 경우 현재 주문폭주로 인한 예약 판매중으로
-                      순차 발송 진행되고 있습니다.
-                      <br />
-                      주문 시 최대한 빠른 발송 진행될 수 있도록 노력하겠습니다.
-                      <br />
-                      감사합니다.
-                    </p>
-                  </div>
-                </div>
-              </article>
-            </li>
-          </ol>
-
-          <div className="pagination">
-            <ol className="page-list">
-              <li className="page-item is-active">
-                <a href="/">1</a>
-              </li>
-              <li className="page-item">
-                <a href="/">2</a>
-              </li>
-              <li className="page-item">
-                <a href="/">3</a>
-              </li>
-              <li className="page-item">
-                <a href="/">4</a>
-              </li>
-              <li className="page-item">
-                <a href="/">5</a>
-              </li>
-            </ol>
-            <button className="page-control page-next" type="button">
-              <i className="ic-chevron"></i>
-            </button>
-          </div>
+          {inquiryList}
+          <Pagination
+            totalCount={productionInquiry.count || 0}
+            countPerPage={5}
+            actionFunction={getProductionInquiry}
+            productId={productions.id}
+            page={page}
+            setPage={setPage}
+          />
         </div>
       </section>
       <div className="product-section-divider sm-only" aria-hidden></div>
     </>
-
-    // {/* <!-- NOTE: 문의가 0개일 경우 -->
-    //       <!-- <section
-    //         className="product-section product-inquiry is-open"
-    //         id="product-inquiry"
-    //         role="tabpanel"
-    //       >
-    //         <header className="product-section-header">
-    //           <h1 className="title">문의</h1>
-    //           <strong className="badge" aria-label="0개">0</strong>
-
-    //           <a className="text-button" href="/">문의하기</a>
-    //           <button
-    //             className="icon-button sm-only"
-    //             type="button"
-    //             aria-label="더보기"
-    //           >
-    //             <i className="ic-chevron" aria-hidden></i>
-    //           </button>
-    //         </header>
-
-    //         <div className="product-section-content">
-    //           <p className="inquiry-empty">문의 내역이 없습니다.</p>
-    //         </div>
-    //       </section>
-    //       <div className="product-section-divider sm-only" aria-hidden></div> --> */}
   );
 };
 
