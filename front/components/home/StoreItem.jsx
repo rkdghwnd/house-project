b8c535792deb9cc2197a79f5b58e7975ef3803a2
rkdghwnd <ghwnd6448@gmail.com>
2023-11-21 01:07:02 +0900

    [feat] StoreItem

--- front/components/home/StoreItem.jsx
+++ front/components/home/StoreItem.jsx
@@ -1,35 +1,60 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const StoreItem = () => {
+const StoreItem = ({
+  id,
+  image_url,
+  brand_name,
+  product_name,
+  review_avg,
+  review_count,
+  selling_price,
+  original_price,
+  free_delivery,
+  is_special_price,
+  is_departure_today,
+}) => {
+  const productDiscount = parseInt(
+    ((original_price - selling_price) / original_price) * 100
+  );
+
   return (
-    <Link to="/productions/1">
+    <Link to={`/productions/${id}`}>
       <div className="store-item">
         <div className="store-item-image">
-          <img src="/assets/images/product/product-thumnail-2.avif" />
+          <img src={image_url} loading="lazy" alt={product_name} />
         </div>
         <div className="store-item-info">
-          <h4>애경</h4>
-          <span className="store-item-info-title">
-            [40%+15%][한정수량] 2080 프리미엄치약 9개입 무료배송 외 초특가!
-          </span>
+          <h4>{brand_name}</h4>
+          <span className="store-item-info-title">{product_name}</span>
           <div className="store-item-info-review sm-only">
             <i className="ic-star is-active"></i>
-            <span>4.7</span>
-            <span>리뷰 5,670</span>
+            <span>{review_avg}</span>
+            <span>리뷰 {review_count}</span>
           </div>
           <div className="store-item-info-discount">
-            <span className="rate">47%</span>
-            <span className="percent">13,500 외</span>
+            {productDiscount !== 0 && (
+              <span className="rate">{productDiscount}%</span>
+            )}
+            <span className="percent">{selling_price?.toLocaleString()}</span>
           </div>
           <div className="store-item-info-review sm-hidden">
             <i className="ic-star is-active"></i>
-            <span>4.7</span>
-            <span>리뷰 5,670</span>
+            <span>{review_avg}</span>
+            <span>리뷰</span>
+            <span>{review_count}</span>
+          </div>
+          <div className="store-item-info-today-delivery">
+            {is_departure_today && (
+              <img
+                src="/assets/images/fast-delivery-tag.png"
+                alt="today_delivery"
+              />
+            )}
           </div>
           <div className="store-item-info-tag">
-            <button className="tag-gray">무료배송</button>
-            <button className="tag-red">특가</button>
+            {free_delivery && <button className="tag-gray">무료배송</button>}
+            {is_special_price && <button className="tag-red">특가</button>}
           </div>
         </div>
       </div>
