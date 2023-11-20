06052f04199c2e523ef44a326a02c216ee8a6253
rkdghwnd <ghwnd6448@gmail.com>
2023-11-21 01:11:24 +0900

    [feat] ReviewScoreBoard

--- front/components/productions/ReviewScoreBoard.jsx
+++ front/components/productions/ReviewScoreBoard.jsx
@@ -1,19 +1,28 @@
 import React from 'react';
 import ScoreStats from './ScoreStats';
+import { useSelector } from 'react-redux';
+import shortid from 'shortid';
 
 const ReviewScoreBoard = () => {
+  const { productions } = useSelector((state) => state.productions);
+  const starCount = Math.round(productions?.review_avg);
+  const starIconCount = new Array(5).fill(starCount);
+
   return (
     <div className="review-scoreboard">
       <div className="score-summary">
-        <strong className="average-score" aria-label="평점 4.8">
-          4.8
+        <strong className="average-score">
+          {productions?.review_avg?.toFixed(2)}
         </strong>
         <div className="star-rating">
-          <i className="ic-star is-active"></i>
-          <i className="ic-star is-active"></i>
-          <i className="ic-star is-active"></i>
-          <i className="ic-star is-active"></i>
-          <i className="ic-star is-active"></i>
+          {starIconCount.map((count) => {
+            return (
+              <i
+                key={shortid.generate()}
+                className={`ic-star${count >= 1 && ' is-active'}`}
+              ></i>
+            );
+          })}
         </div>
       </div>
 
