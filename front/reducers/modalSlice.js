44c2bc777020eb2befcdf3f29c507bf7040c2994
rkdghwnd <ghwnd6448@gmail.com>
2023-11-21 01:19:14 +0900

    [feat] modalSlice

--- front/reducers/modalSlice.js
+++ front/reducers/modalSlice.js
@@ -1,5 +1,4 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { addPost } from '../actions/post';
 
 const initialState = {
   sideBarVisible: false,
@@ -7,31 +6,29 @@ const initialState = {
   orderFormModalVisible: false,
   cartModalVisible: false,
   logInModalVisibile: false,
+  filterModalVisible: false,
+  shareModalVisible: false,
+  messageModalVisible: false,
+  message: '',
+  copyToClipMessageVisible: false,
+  withdrawModalVisible: false,
+  writingReviewFormVisible: false,
+  writingInquiryFormVisible: false,
+  imagePreviewVisible: false,
+  removeReviewConfirmModalVisible: false,
+  removeReviewId: null,
+  removeReviewProductId: null,
+  writingReviewFormMode: '',
+  writingInquiryFormMode: '',
+  removeInquiryConfirmModalVisible: false,
+  removeInquiryId: null,
+  removeInquiryProductId: null,
   overlay: false,
 };
 
 const modalSlice = createSlice({
   name: 'modal',
   initialState,
-  // reducers, extraReducers 에는 immer가 적용되어 있다.
-  // action에 대한 데이터는 무조건 payload로 통일되어있다.
-  // 주의 : state를 통째로 바꾸는 경우(state = 123) immer의 불변성이 깨진다
-  // 해결법 : state를 바꿨으면 끝에 return state 를 해줘야 한다.
-
-  // extraReducers에 addCase 쓰는 이유 : 메서드 정의하는 방식으로 작성하면
-  // 타입스크립트 사용시 타입추론이 힘들다.
-
-  // 메서드 정의하는 방식
-  // extraReducers : {
-  //     state = 123
-  //   },
-  //   [addPost.filfilled](state, actopm) {
-  //     state.data.push(action.payload);
-  //   },
-  //   [addPost.rejected](state, action) {
-  //   }
-  // }
-
   reducers: {
     // 동기적 실행인 리듀서
     openSideBar(state, action) {
@@ -54,43 +51,82 @@ const modalSlice = createSlice({
       state.logInModalVisibile = true;
       state.overlay = true;
     },
+    openFilterModal(state, action) {
+      state.filterModalVisible = true;
+      state.overlay = true;
+    },
+    openShareModal(state, action) {
+      state.shareModalVisible = true;
+      state.overlay = true;
+    },
+    openMessageModal(state, action) {
+      state.messageModalVisible = true;
+      state.overlay = true;
+      state.message = action.payload.message;
+      state.withdrawModalVisible = false;
+    },
+    closeMessageModal(state, action) {
+      state.messageModalVisible = false;
+      state.orderFormModalVisible = false;
+      state.overlay = false;
+      state.message = action.payload.message;
+    },
+    openCopyToClipMessage(state, action) {
+      state.copyToClipMessageVisible = true;
+    },
+    closeCopyToClipMessage(state, action) {
+      state.copyToClipMessageVisible = false;
+    },
+    openWithdrawModal(state, action) {
+      state.withdrawModalVisible = true;
+      state.overlay = true;
+    },
+    openWritingReviewForm(state, action) {
+      state.writingReviewFormVisible = true;
+      state.writingReviewFormMode = action.payload.mode;
+      if (action.payload.mode === 'update') {
+        state.imagePreviewVisible = true;
+      }
+      state.overlay = true;
+    },
+    updateImagePreview(state, action) {
+      state.imagePreviewVisible = action.payload.visible;
+    },
+    openWritingInquiryForm(state, action) {
+      state.writingInquiryFormVisible = true;
+      state.writingInquiryFormMode = action.payload.mode;
+      state.overlay = true;
+    },
+    openRemoveReviewConfirmModal(state, action) {
+      state.removeReviewConfirmModalVisible = true;
+      state.removeReviewId = action.payload.reviewId;
+      state.removeReviewProductId = action.payload.productId;
+      state.overlay = true;
+    },
+    openRemoveInquiryConfirmModal(state, action) {
+      state.removeInquiryConfirmModalVisible = true;
+      state.removeInquiryId = action.payload.inquiryId;
+      state.removeInquiryProductId = action.payload.productId;
+      state.overlay = true;
+    },
     closeModal(state, action) {
       state.sideBarVisible = false;
       state.searchModalVisible = false;
       state.orderFormModalVisible = false;
       state.cartModalVisible = false;
       state.logInModalVisibile = false;
+      state.filterModalVisible = false;
+      state.shareModalVisible = false;
+      state.withdrawModalVisible = false;
+      state.writingReviewFormVisible = false;
+      state.writingInquiryFormVisible = false;
+      state.removeReviewConfirmModalVisible = false;
+      state.removeInquiryConfirmModalVisible = false;
+      state.messageModalVisible = false;
       state.overlay = false;
     },
   },
-  extraReducers: (builder) =>
-    builder
-      .addCase(addPost.pending, (state, action) => {})
-      .addCase(addPost.fulfilled, (state, action) => {
-        state.list.push(action.payload);
-      })
-      .addCase(addPost.rejected, (state, action) => {})
-      .addMatcher(
-        (action) => {
-          return action.type.includes('/pending');
-          // 뒤에 pending이 붙은 것들은 뒤의 콜백에서 처리
-          // 공통적인 상태변화를 한번에 처리할 수 있는 패턴
-          // 예시
-          // swtich() {
-          //   CASE 'add_pending':
-          //   CASE 'update_pending':
-          //     state.isLoading = true;
-          //     break;
-          // }
-          // 이러한 방식으로 처리하는 것과 같다.
-        },
-        (state, action) => {
-          state.isLoading = true;
-        }
-      )
-      .addDefaultCase((state, action) => {
-        // switch문의 default에 해당하는 케이스
-      }),
+  extraReducers: (builder) => builder,
 });
 
 export default modalSlice;
