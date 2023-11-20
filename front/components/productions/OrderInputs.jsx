33f75315f6a2b3154977b6b8182f6f6058dc5408
rkdghwnd <ghwnd6448@gmail.com>
2023-11-21 01:09:17 +0900

    [feat] OrderInputs

--- front/components/productions/OrderInputs.jsx
+++ front/components/productions/OrderInputs.jsx
@@ -1,27 +1,42 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
+import shortid from 'shortid';
+import { useDispatch, useSelector } from 'react-redux';
+import productionsSlice from '../../reducers/productionsSlice';
 
 const OrderInputs = () => {
+  const dispatch = useDispatch();
+  const { productionOptions } = useSelector((state) => state.productions);
+
+  const onChangeOptions = useCallback((e) => {
+    // 선택한 옵션 추가
+    const optionIndex = parseInt(e.currentTarget.value);
+    if (optionIndex !== 0) {
+      dispatch(
+        productionsSlice.actions.plusSelectedOptionCount({
+          optionIndex,
+        })
+      );
+    }
+    e.stopPropagation();
+  }, []);
+
   return (
     <div className="order-inputs">
       <div className="select-group is-active">
-        <select className="form-select" id="select-1" required>
-          <option value="">선택</option>
-          <option value="1">선택사항 1</option>
-          <option value="2">선택사항 2</option>
-          <option value="3">선택사항 3</option>
-          <option value="4">선택사항 4</option>
-          <option value="5">선택사항 5</option>
-        </select>
-        <i className="ic-caret" aria-hidden></i>
-      </div>
-      <div className="select-group">
-        <select className="form-select" id="select-2">
-          <option value="">추가상품 (선택)</option>
-          <option value="1">선택사항 1</option>
-          <option value="2">선택사항 2</option>
-          <option value="3">선택사항 3</option>
-          <option value="4">선택사항 4</option>
-          <option value="5">선택사항 5</option>
+        <select
+          className="form-select"
+          id="select-1"
+          required
+          onChange={onChangeOptions}
+        >
+          <option value={0}>선택</option>
+          {productionOptions.map((option, id) => {
+            return (
+              <option key={shortid.generate()} value={id + 1}>
+                {option}
+              </option>
+            );
+          })}
         </select>
         <i className="ic-caret" aria-hidden></i>
       </div>
