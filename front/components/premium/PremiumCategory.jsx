bcf2504063dcb18ffb5b3b4768b4be871cf13eed
rkdghwnd <ghwnd6448@gmail.com>
2023-11-21 01:07:59 +0900

    [feat] PremiumCategory

--- front/components/premium/PremiumCategory.jsx
+++ front/components/premium/PremiumCategory.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { premiumCategorys } from '../../hooks/premium';
+import { premiumCategorys } from '../../datas/premium';
 import shortid from 'shortid';
 
 const PremiumCategory = () => {
