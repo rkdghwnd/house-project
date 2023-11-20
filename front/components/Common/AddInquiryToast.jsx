e29302949e9fe03dba9dc6dccfa3f44f718670cc
rkdghwnd <ghwnd6448@gmail.com>
2023-11-21 00:16:04 +0900

    [feat] AddInquiryToast

---
+++ front/components/Common/AddInquiryToast.jsx
@@ -0,0 +1,44 @@
+import React, { useCallback, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import toastSlice from '../../reducers/toastSlice';
+import { Link } from 'react-router-dom';
+
+const AddInquiryToast = () => {
+  const dispatch = useDispatch();
+  const { me } = useSelector((state) => state.user);
+
+  const closeToast = useCallback(() => {
+    dispatch(toastSlice.actions.closeToast());
+  }, []);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      dispatch(toastSlice.actions.closeToast());
+    }, 3000);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
+
+  return (
+    <aside className={`inquiry-toast`}>
+      <h1 className="inquiry-title">문의글을 작성했습니다.</h1>
+      <button
+        className="close-button"
+        type="button"
+        aria-label="닫기"
+        onClick={closeToast}
+      >
+        <i className="ic-close" aria-hidden></i>
+      </button>
+
+      <div className="button-group">
+        <Link className="btn-32 btn-outlined" to={`/my_shopping/inquiry`}>
+          목록 보러가기
+        </Link>
+      </div>
+    </aside>
+  );
+};
+
+export default AddInquiryToast;
